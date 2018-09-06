6705493a73297af03b8f63189e2b040ef9ddfe04
Alex Preissler <AlexJPreissler@Outlook.com>
2018-09-06 17:33:21 -0400

    update

--- db/orm.js
+++ db/orm.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
 	host: 'localhost',
-	user: 'root',
-	password: 'root',
-	database: 'dbUsers',
+	user: 'Alex-Preissler',
+	password: 'Paintball426*',
+	database: 'clashdb',
 	port:3306,
 })
 
